01a97e5dd49b80339be0c1371ed28c399d13aa4c
Edward Hubbard <edhubbard1994@gmail.com>
2021-11-01 03:16:52 +0000

    mpi sum numbers

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 #include <memory>
 #include "divider.h"
 
-#define DATA_SIZE 119
-#define NUM_PARTITIONS 29
+#define DATA_SIZE 50000
+#define NUM_PARTITIONS 20
 #define START_RANGE 0
-#define END_RANGE 100
+#define END_RANGE 10
 
 int main (int argv, char **argc) {
 	int rank;
@@ -28,6 +28,7 @@ int main (int argv, char **argc) {
 				proc_count = 1;
 			}
 		}
+
 		
 	} else {
 	// worker process
@@ -35,7 +36,24 @@ int main (int argv, char **argc) {
 		std::vector<int> v(DATA_SIZE / NUM_PARTITIONS);
 		MPI_Recv(&v[0],(DATA_SIZE / NUM_PARTITIONS), MPI_INT, 0 ,1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		std::cout << "Received data of size: " << v.size() << " \n";
+		int acc = 0;
+		for (int x : v) {
+			acc += x;
+		}
+		MPI_Send( &acc, 1, MPI_INT, 1, 1, MPI_COMM_WORLD);
 	}
+
+	int proc_count = 1;
+	int acc = 0;
+	while (proc_count < 20 ) {
+		int p;
+		MPI_Recv(&p,1, MPI_INT, proc_count, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		std::cout << "Local Total: " << p << " \n";
+		++proc_count;
+		acc += p;
+	}
+	std::cout << "===============\n\n";
+	std::cout << " THE GRAND TOTAL = " << acc << "\n\n";
 	MPI_Finalize();
 
 
